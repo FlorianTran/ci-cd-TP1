3d32b10ca94bf5649b095811b3a4c9fb8b25a4fe
FlorianTran <florian.tranpro@gmail.com>
2025-06-20 16:04:24 +0200

    Add end-to-end testing and refactor for ES module syntax

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function sum(a, b) {
+export function sum(a, b) {
   return a + b;
 }
 
-export default sum;
+export function multiply(a, b) {
+  return a * b;
+}
--- index.test.js
+++ index.test.js
@@ -1,5 +1,4 @@
-import sum from './index';
-import { test, expect } from '@jest/globals';
+import {sum, multiply} from './index.js';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -7,4 +6,8 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('adds -1 + 1 to equal 0', () => {
   expect(sum(-1, 1)).toBe(0);
+});
+
+test('multiplies 2 * 3 to equal 6', () => {
+  expect(multiply(2, 3)).toBe(6);
 });
