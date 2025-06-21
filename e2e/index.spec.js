222b791806409be10a4c6d95a72014fc87bee9cb
FlorianTran <florian.tranpro@gmail.com>
2025-06-21 15:33:29 +0200

    Add sum function test in Playwright end-to-end test

--- e2e/index.spec.js
+++ e2e/index.spec.js
@@ -1,11 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { sum } from '../index.js';
 
 test('sum function in browser context', async ({ page }) => {
+  const result = sum(3, 4);
   await page.setContent(`
     <script type="module">
-      window.result = (3 + 4).toString();
+      window.result = '${result}';
       document.title = window.result;
     </script>
   `);
-  await expect(page).toHaveTitle('7');``
+  await expect(page).toHaveTitle('7');
 });
